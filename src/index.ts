e201ce123bc691ded6d25bcb8dcdc8fbee68a467
sagar-pardhi <sgrpardhi007@gmail.com>
2023-12-15 20:24:45 +0530

    remove bodyparser package

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
 import express, { Request, Response } from "express";
-import bodyParser from "body-parser";
 
 export const app = express();
 
-app.use(bodyParser({}));
+app.use(express.json());
 
 interface Balances {
   [key: string]: number;
